7cb28bbd78bcdbd18c65122104048d24bfe35fbf
lazoliver <wilian.oliveiractt@gmail.com>
2021-09-15 02:07:50 -0300

    Server Listening

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.post('/mail', (req, res) => {
     })
 })
 
+const hostname = 'localhost';
+const port = '3000';
+
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+    console.log(`Server listening on http://${hostname}:${port}/`);
 });
